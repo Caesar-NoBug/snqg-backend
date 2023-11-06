cede7ea20707fdfbf5cc2204269bfe9a07844973
Coder-Caesar <caesar666233@qq.com>
2023-11-06 18:47:26 +0800

    实现上传文件、获取文件功能

--- children/src/main/java/com/snqg/point/domain/dto/point/request/RankingRequest.java
+++ children/src/main/java/com/snqg/point/domain/dto/point/request/RankingRequest.java
@@ -5,8 +5,8 @@ import lombok.Data;
 
 @Data
 public class RankingRequest {
-    @ApiModelProperty(value = "timeRange", example = "total")
+    @ApiModelProperty(value = "时间范围（total:总积分，year:年度积分，month:月度积分，week:周积分）", example = "total")
     private String timeRange;
-    @ApiModelProperty(value = "rankingRange", example = "group")
+    @ApiModelProperty(value = "排序范围（system:系统范围内进行排名，group:小组范围内进行排名）", example = "group")
     private String rankingRange;
 }
--- domain/src/main/java/com/snqg/domain/response/children/LoginResponse.java
+++ domain/src/main/java/com/snqg/domain/response/children/LoginResponse.java
@@ -9,7 +9,7 @@ import java.util.Date;
 @Data
 public class LoginResponse {
 
-    @ApiModelProperty(value = "认证token，后续请求都使用这个token来进行认证,这个token是可用的测试token", example = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI0N2E3YzQ0MWVmYjg0OGFjOTI4MTFlNTAwNDkyYTE5MSIsInN1YiI6Im8ySEVONWExLTYyQUwxU0RjMThnNGxXN1BRVWciLCJpc3MiOiJzZyIsImlhdCI6MTY5OTAxMTEyMCwiZXhwIjoxNjk5MDE0NzIwfQ.3yAo8ID_R6iLYEI2FoWU3VbJ0YQjXXcTq6FxAzxNQRE")
+    @ApiModelProperty(value = "认证token，后续请求都使用这个token来进行认证", example = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI0N2E3YzQ0MWVmYjg0OGFjOTI4MTFlNTAwNDkyYTE5MSIsInN1YiI6Im8ySEVONWExLTYyQUwxU0RjMThnNGxXN1BRVWciLCJpc3MiOiJzZyIsImlhdCI6MTY5OTAxMTEyMCwiZXhwIjoxNjk5MDE0NzIwfQ.3yAo8ID_R6iLYEI2FoWU3VbJ0YQjXXcTq6FxAzxNQRE")
     private String token;
 
     @ApiModelProperty(value = "用户id", example = "o2HEN5a1-62AL1SDc18g4lW7PQUg")
